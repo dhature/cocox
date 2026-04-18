4eb88a18a13e62d05f2bc433bdaec44573752a00
thapa-bipin <tailung42@gmail.com>
2026-04-18 15:20:05 +0545

    feat: lint commit messages from a range of hashes

--- src/git_helpers.rs
+++ src/git_helpers.rs
@@ -14,3 +14,43 @@ pub fn get_commit_message_from_hash(commit_hash: &str) -> Result<String> {
     let message = String::from_utf8_lossy(&output.stdout).trim().to_string();
     Ok(message)
 }
+
+pub fn get_commit_messages_from_hash_range(from_hash: &str, to_hash: &str) -> Result<Vec<String>> {
+    let range = format!("{}..{}", from_hash, to_hash);
+
+    // range A..B returns all commits from B but not A ie (B - A), so A has to added seaprately
+    let from_hash_message = get_commit_message_from_hash(from_hash)?;
+
+    let output = Command::new("git")
+        .args([
+            "log",
+            "--no-merges",
+            "--pretty=format:%s",
+            "--reverse",
+            &range,
+        ])
+        .output()
+        .with_context(|| {
+            format!(
+                "failed to execute git log for range {}, {}",
+                from_hash, to_hash
+            )
+        })?;
+
+    if !output.status.success() {
+        anyhow::bail!(
+            "failed to retrieve commit messages for range {}, {}",
+            from_hash,
+            to_hash
+        );
+    }
+
+    let mut messages: Vec<String> = String::from_utf8_lossy(&output.stdout)
+        .lines()
+        .map(|line| line.to_string())
+        .collect();
+    // prepend the first message of the range
+    messages.insert(0, from_hash_message);
+
+    Ok(messages)
+}
